ee93cefc678984e33f7382453e0dda15ddb0e55a
Mohamed Amine BENSAFTA <mohamedamine.bensafta@sesame.com.tn>
2023-10-17 14:09:05 +0100

    first commit

---
+++ src/main/java/com/demotp3devops/Demotp3devopsApplication.java
@@ -0,0 +1,13 @@
+package com.demotp3devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotp3devopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demotp3devopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demotp3devops/Demotp3devopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demotp3devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demotp3devopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
